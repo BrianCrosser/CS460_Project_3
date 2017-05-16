0aa247589e45ce02c832869fbc108dfa4a6d7bfb
Jordan Bergero <bergero@sonoma.edu>
2017-05-16 14:44:46 -0700

    prepping merge

---
+++ testFiles/9_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Object.h"
+
+using namespace std;
+
+Object removeFirst(Object list){
+    Object _retVal; 
+
+    cout << listcar(;
+    cout << endl;
+
+	return _retVal;
+}
+
+int main(){
+    Object _retVal; 
+
+    removeFirst();
+
+	return 0;
+}
